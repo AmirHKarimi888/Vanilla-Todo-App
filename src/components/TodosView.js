3d6a9bd977cdef0ce05b918b797225f4f5308977
AmirHK888 <amirhkarimi888@gmail.com>
2024-01-27 12:59:23 +0330

    Logical actions have been defiened

--- src/components/TodosView.js
+++ src/components/TodosView.js
@@ -1,3 +1,4 @@
+import { addTodo, clearCompleted, deleteTodo, setFilter, setTodoCompletionState } from "../main.js";
 import { state } from "../model.js";
 
 class TodosView {
@@ -7,13 +8,58 @@ class TodosView {
     }
 
     main = document.querySelector("main");
+    todoInput = "";
 
     render() {
         this.generateMarkup();
+        this.eventHandler();
     }
 
     eventHandler() {
+        const todoInputEl = document.querySelector("#todoInput");
+        const todoInitialCheckBtnEl = document.querySelector("#todoInitialCheckBtn");
+        const deleteTodoBtnsEl = document.querySelectorAll(".delete-todo-btn");
+        const filtersEl = document.querySelectorAll(".card-footer-filter-item");
+        const setTodoCompletionBtnEl = document.querySelectorAll(".set-todo-completion-btn");
+        const clearCompletedBtn = document.querySelector(".card-footer-clear-compeleted-btn");
 
+        todoInputEl.addEventListener("change", (event) => {
+           this.todoInput = event.target.value;
+        })
+
+        todoInputEl.addEventListener("keyup", (event) => {
+            if(event.key === "Enter") {
+                addTodo(this.todoInput, todoInitialCheckBtnEl.checked);
+                this.render();
+            }
+        })
+
+
+        deleteTodoBtnsEl.forEach(btn => {
+            btn.addEventListener("click", () => {
+                deleteTodo(btn.id.slice(btn.id.length - 1));
+                this.render();
+            });
+        })
+
+        filtersEl.forEach(filter => {
+            filter.addEventListener("click", () => {
+                setFilter(filter.id);
+                this.render();
+            });
+        })
+
+        setTodoCompletionBtnEl.forEach(btn => {
+            btn.addEventListener("click", () => {
+                setTodoCompletionState(btn.id.slice(btn.id.length - 1), btn.checked);
+                this.render();
+            })
+        })
+
+        clearCompletedBtn.addEventListener("click", () => {
+            clearCompleted();
+            this.render();
+        })
     }
 
     generateMarkup() {
@@ -27,8 +73,8 @@ class TodosView {
   
         <div class="card-body">
           <div class="card-body-input">
-            <input type="checkbox">
-            <input type="text" placeholder="Create a new todo...">
+            <input type="checkbox" id="todoInitialCheckBtn">
+            <input type="text" placeholder="Create a new todo..." id="todoInput">
           </div>
   
           <ul class="card-body-items">
@@ -36,12 +82,12 @@ class TodosView {
                 state.displayingTodos.length !== 0 ? state.displayingTodos.map(todo => /*html*/`
                 <li class="card-body-item">
                 <div class="card-body-item-checking">
-                  <span class="card-body-item-checkbox"><input type="checkbox" ${ todo?.completed ? "checked" : null }></span>
-                  <span class="card-body-item-title">${ todo?.title }</span>
+                  <span class="card-body-item-checkbox"><input type="checkbox" class="set-todo-completion-btn" id="setTodoCheckBtn${ todo?.id }" ${ todo?.completed ? "checked" : null }></span>
+                  <span class="card-body-item-title" style="${ todo?.completed ? 'text-decoration: line-through; color: hsl(233, 11%, 84%);' : '' }">${ todo?.title }</span>
                 </div>
     
                 <div class="card-body-item-deleting">
-                  <span class="card-body-item-delete-btn"><img src="./src/assets/images/icon-cross.svg" alt="delete" id="deleteTodoBtn${ todo?.id }"></span>
+                  <span class="card-body-item-delete-btn"><img src="./src/assets/images/icon-cross.svg" alt="delete" class="delete-todo-btn" id="deleteTodoBtn${ todo?.id }"></span>
                 </div>
               </li>`).join("") : /*html*/`
               <p style="padding: 2rem; text-align: center;">Your todo list is empty!</p>
@@ -53,9 +99,9 @@ class TodosView {
         <div class="card-footer">
           <div class="card-footer-items-left"><span>${ state.displayingTodos.length } items left</span></div>
           <ul class="card-footer-filter-items">
-            <li class="card-footer-filter-item" style="${ state.filter === "all" ? "color: hsl(220, 98%, 61%);" : '' }"><span>All</span></li>
-            <li class="card-footer-filter-item" style="${ state.filter === "active" ? "color: hsl(220, 98%, 61%);" : '' }"><span>Active</span></li>
-            <li class="card-footer-filter-item" style="${ state.filter === "completed" ? "color: hsl(220, 98%, 61%);" : '' }"><span>Completed</span></li>
+            <li class="card-footer-filter-item" id="all" style="${ state.filter === "all" ? "color: hsl(220, 98%, 61%);" : '' }"><span>All</span></li>
+            <li class="card-footer-filter-item" id="active" style="${ state.filter === "active" ? "color: hsl(220, 98%, 61%);" : '' }"><span>Active</span></li>
+            <li class="card-footer-filter-item" id="completed" style="${ state.filter === "completed" ? "color: hsl(220, 98%, 61%);" : '' }"><span>Completed</span></li>
           </ul>
           <div class="card-footer-clear-compeleted-btn"><span>Clear Completed</span></div>
         </div>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,92 @@
 import TodosView from "./components/TodosView.js";
 import { state } from "./model.js";
 
+if("todos" in localStorage) {
+    localStorage.getItem("todos") === "" ? localStorage.setItem("todos", "[]") : null;
+    state.todos = JSON.parse(localStorage.getItem("todos"));
+} else {
+    localStorage.setItem("todos", "[]");
+    state.todos = JSON.parse(localStorage.getItem("todos"));
+}
+
+
 state.displayingTodos = state.todos;
 
 TodosView.render();
+
+export const setFilter = (filter) => {
+    state.filter = filter;
+
+    if(filter === "all") {
+        state.displayingTodos = state.todos;
+
+    } else if(filter === "active") {
+        state.displayingTodos = state.todos.filter(todo => {
+            if(!todo?.completed) {
+                return todo;
+            }
+        })
+
+    } else if(filter === "completed") {
+        state.displayingTodos = state.todos.filter(todo => {
+            if(todo?.completed) {
+                return todo;
+            }
+        })
+    }
+}
+
+export const setTodoCompletionState = (id, completed) => {
+    state.todos.filter(todo => {
+        if(todo?.id === +id) {
+            todo.completed = completed;
+        }
+    })
+
+    localStorage.setItem("todos", JSON.stringify(state.todos));
+    state.displayingTodos = state.todos;
+}
+
+export const clearCompleted = () => {
+    state.todos = state.todos.filter(todo => {
+        if(!todo?.completed) {
+            return todo;
+        }
+    })
+
+    localStorage.setItem("todos", JSON.stringify(state.todos));
+    state.displayingTodos = state.todos;
+}
+
+export const addTodo = (title, completed) => {
+    let lastTodo = state.todos.sort((a, b) => a - b)[state.todos.length - 1];
+
+    if(state.todos.length === 0) {
+        state.todos = [...state.todos, {
+            id: 1,
+            title: title,
+            completed: completed
+        }]
+
+    } else {
+        state.todos = [...state.todos, {
+            id: lastTodo?.id + 1,
+            title: title,
+            completed: completed
+        }]
+    }
+
+    localStorage.setItem("todos", JSON.stringify(state.todos));
+    state.displayingTodos = state.todos;
+}
+
+export const deleteTodo = (id) => {
+    state.todos = state.todos.filter(todo => {
+        if(todo?.id !== +id) {
+            return todo;
+        }
+    })
+
+    localStorage.setItem("todos", JSON.stringify(state.todos));
+    state.displayingTodos = state.todos;
+}
